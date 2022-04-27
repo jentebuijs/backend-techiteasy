1b3b154ee288fe9a5814d50dc7ae76c57c509063
jentebuijs <jentebuijs@gmail.com>
2022-04-27 20:32:31 +0200

    Controller opdracht af

--- src/main/java/nl/novi/backend/techiteasy/controllers/ExceptionController.java
+++ src/main/java/nl/novi/backend/techiteasy/controllers/ExceptionController.java
@@ -1,4 +1,16 @@
 package nl.novi.backend.techiteasy.controllers;
 
+import nl.novi.backend.techiteasy.exceptions.TelevisionNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> exception (TelevisionNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
