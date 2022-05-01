65a45f2b14c4b49030d78e2baac30fcd64fb66c2
jentebuijs <jentebuijs@gmail.com>
2022-05-01 21:45:05 +0200

    service-dto verbeterd

--- src/main/java/nl/novi/backend/techiteasy/models/dtos/TelevisionInputDto.java
+++ src/main/java/nl/novi/backend/techiteasy/models/dtos/TelevisionInputDto.java
@@ -3,158 +3,40 @@ package nl.novi.backend.techiteasy.models.dtos;
 import nl.novi.backend.techiteasy.models.entities.Television;
 
 public class TelevisionInputDto {
-    private String type;
-    private String brand;
-    private Double price;
-    private Double availableSize;
-    private Double refreshRate;
-    private String screenType;
-    private String screenQuality;
-    private Boolean smartTv;
-    private Boolean wifi;
-    private Boolean voiceControl;
-    private Boolean hdr;
-    private Boolean bluetooth;
-    private Boolean ambiLight;
-    private Integer originalStock;
-    private Integer sold;
+    public String type;
+    public String brand;
+    public Double price;
+    public Double availableSize;
+    public Double refreshRate;
+    public String screenType;
+    public String screenQuality;
+    public Boolean smartTv;
+    public Boolean wifi;
+    public Boolean voiceControl;
+    public Boolean hdr;
+    public Boolean bluetooth;
+    public Boolean ambiLight;
+    public Integer originalStock;
+    public Integer sold;
 
     public static Television toTelevision(TelevisionInputDto televisionInputDto){
-        Television television = new Television(televisionInputDto.getType(),
-                televisionInputDto.getBrand(),
-                televisionInputDto.getPrice(),
-                televisionInputDto.getAvailableSize(),
-                televisionInputDto.getRefreshRate(),
-                televisionInputDto.getScreenType(),
-                televisionInputDto.getScreenQuality(),
-                televisionInputDto.getSmartTv(),
-                televisionInputDto.getWifi(),
-                televisionInputDto.getVoiceControl(),
-                televisionInputDto.getHdr(),
-                televisionInputDto.getBluetooth(),
-                televisionInputDto.getAmbiLight(),
-                televisionInputDto.getOriginalStock(),
-                televisionInputDto.getSold());
+        Television television = new Television();
+        television.setType(televisionInputDto.type);
+        television.setBrand(televisionInputDto.brand);
+        television.setPrice(televisionInputDto.price);
+        television.setAvailableSize(televisionInputDto.availableSize);
+        television.setRefreshRate(televisionInputDto.refreshRate);
+        television.setScreenType(televisionInputDto.screenType);
+        television.setScreenQuality(televisionInputDto.screenQuality);
+        television.setSmartTv(televisionInputDto.smartTv);
+        television.setWifi(televisionInputDto.wifi);
+        television.setVoiceControl(televisionInputDto.voiceControl);
+        television.setHdr(televisionInputDto.hdr);
+        television.setBluetooth(televisionInputDto.bluetooth);
+        television.setAmbiLight(televisionInputDto.ambiLight);
+        television.setOriginalStock(televisionInputDto.originalStock);
+        television.setSold(televisionInputDto.sold);
         return television;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Double getAvailableSize() {
-        return availableSize;
-    }
-
-    public void setAvailableSize(Double availableSize) {
-        this.availableSize = availableSize;
-    }
-
-    public Double getRefreshRate() {
-        return refreshRate;
-    }
-
-    public void setRefreshRate(Double refreshRate) {
-        this.refreshRate = refreshRate;
-    }
-
-    public String getScreenType() {
-        return screenType;
-    }
-
-    public void setScreenType(String screenType) {
-        this.screenType = screenType;
-    }
-
-    public String getScreenQuality() {
-        return screenQuality;
-    }
-
-    public void setScreenQuality(String screenQuality) {
-        this.screenQuality = screenQuality;
-    }
-
-    public Boolean getSmartTv() {
-        return smartTv;
-    }
-
-    public void setSmartTv(Boolean smartTv) {
-        this.smartTv = smartTv;
-    }
-
-    public Boolean getWifi() {
-        return wifi;
-    }
-
-    public void setWifi(Boolean wifi) {
-        this.wifi = wifi;
-    }
-
-    public Boolean getVoiceControl() {
-        return voiceControl;
-    }
-
-    public void setVoiceControl(Boolean voiceControl) {
-        this.voiceControl = voiceControl;
-    }
-
-    public Boolean getHdr() {
-        return hdr;
-    }
-
-    public void setHdr(Boolean hdr) {
-        this.hdr = hdr;
-    }
-
-    public Boolean getBluetooth() {
-        return bluetooth;
-    }
-
-    public void setBluetooth(Boolean bluetooth) {
-        this.bluetooth = bluetooth;
-    }
-
-    public Boolean getAmbiLight() {
-        return ambiLight;
-    }
-
-    public void setAmbiLight(Boolean ambiLight) {
-        this.ambiLight = ambiLight;
-    }
-
-    public Integer getOriginalStock() {
-        return originalStock;
-    }
-
-    public void setOriginalStock(Integer originalStock) {
-        this.originalStock = originalStock;
-    }
-
-    public Integer getSold() {
-        return sold;
-    }
-
-    public void setSold(Integer sold) {
-        this.sold = sold;
-    }
 }
